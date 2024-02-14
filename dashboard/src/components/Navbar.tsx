093a8ba460e89a01dd914008055af717f4553842
itsbluejelly <ronniedenzel0@gmail.com>
2024-02-14 19:13:03 +0300

    added navbar and footer

---
+++ dashboard/src/components/Navbar.tsx
@@ -0,0 +1,8 @@
+"use client"
+
+// A FUNCTION THAT GENERATES A NAVBAR COMPONENT
+export default function Navbar() {
+  return (
+    <nav className="w-[100]bg-transparent h-[90px]">Navbar</nav>
+  )
+}
