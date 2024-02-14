71e49546b3c9dcf8103a7d1476091054d505c244
mosesmkrs <karisamoses392@gmail.com>
2024-02-14 11:32:14 +0300

    legal policy page

--- dashboard/src/app/page.tsx
+++ dashboard/src/app/page.tsx
@@ -1,6 +1,14 @@
 import Image from "next/image";
+import Link from "next/link";
+
+
 
 export default function Home() {
   return (
+    <>
     <div>Birble</div>
+    <Link href={'/legalpolicy'}>Legal Policy</Link>
+    </>
+    
+    
   )};
