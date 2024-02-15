c181060cdabc683908660a16ddc004352af69396
Ray-Ezra <134621588+Ray-Ezra@users.noreply.github.com>
2024-02-15 10:54:36 +0300

    Update page.jsx
    Bug fix

--- dashboard/src/app/404/page.jsx
+++ dashboard/src/app/404/page.jsx
@@ -9,7 +9,7 @@ const Background = () => {
         height="30"
         className="fixed top-0 left-0 w-full h-full"
         style={{ color: "transparent", zIndex: 1 }}
-        src="/stars.png"
+        src="/Stars.png"
       />
       <img
         loading="lazy"
@@ -17,7 +17,7 @@ const Background = () => {
         height="30"
         className="fixed top-0 left-0 w-full h-full"
         style={{ color: "transparent", zIndex: 1 }}
-        src="/small planets.png"
+        src="/Small Planets.png"
       />
 
       <img
