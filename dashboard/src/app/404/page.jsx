06ddf5cc935a26a0bbb3beb6ad1781ac0a28ee83
Ray-Ezra <raymondezra276@gmail.com>
2024-02-15 17:06:02 +0300

    change

--- dashboard/src/app/404/page.jsx
+++ dashboard/src/app/404/page.jsx
@@ -7,13 +7,15 @@ const Background = () => {
         loading="lazy"
         className="fixed top-0 left-0 w-full h-full"
         style={{ color: "transparent", zIndex: 1 }}
-        src="/Stars.png"
+        src="/stars.png"
+        alt="Stars"
       />
       <img
         loading="lazy"
         className="fixed top-0 left-0 w-full h-full"
         style={{ color: "transparent", zIndex: 1 }}
-        src="/Small Planets.png"
+        src="/small planets.png"
+        alt="Small Planets"
       />
 
       <img
@@ -31,29 +33,28 @@ const Background = () => {
         alt="Top Right Gradient"
       />
 
-<div className="absolute inset-0 flex justify-center items-center md:justify-center md:items-center">
+      <div className="absolute inset-0 flex justify-center items-center md:justify-center md:items-center"> {/* Center the image in larger screens */}
         <img
           loading="lazy"
-          className="max-w-full h-auto transform rotate-90 md:rotate-0 md:max-w-full md:h-auto transition-transform duration-500 z-10" 
+          className="max-w-full h-auto transform rotate-90 md:rotate-0 md:max-w-full md:h-auto transition-transform duration-500 z-10" // Added z-index
           style={{ color: "transparent", zIndex: 1, maxWidth: '50vw' }}
           src="/404.png"
           alt="404"
         />
       </div>
 
-      {/* Big planet */}
-      <div className=' relative overflow-hidden h-screen flex justify-center items-center z-1'> 
+      <div className='relative overflow-hidden h-screen flex justify-center items-center z-1'>
         <img
           loading="lazy"
-          className="absolute bottom-[-26%] mt-[-30%] transform -translate-y-24 mx-auto transition-transform md:translate-y-0 md:mt[-18%]"
-          style={{ color: "transparent", zIndex: 1, maxWidth: '40vw' }}
+          className="absolute bottom-[-57%] mt-[-30%] transform -translate-y-24 mx-auto transition-transform md:translate-y-0 md:mt[-18%]"
+          style={{ color: "transparent", zIndex: 1, maxWidth: '65vw' }}
           src="/Big Planet.png"
           alt="Big Planet"
         />
       </div>
 
 
-      <div className="absolute inset-0 flex justify-center items-center md:justify-center md:items-center"> 
+      <div className="absolute inset-0 flex justify-center items-center md:justify-center md:items-center">
         <img
           loading="lazy"
           className="max-w-full mt-[5%] h-auto transition-transform md:max-w-full md:h-auto" 
@@ -63,12 +64,12 @@ const Background = () => {
         />
       </div>
 
-      <div className="absolute inset-0  flex mx-auto items-center justify-center z-10"> 
-        <h1 className='text-white pointer-events-none  text-3xl md:text-center' style={{ marginTop: '-8%', marginBottom: '20%' }}>Page Not Found</h1>
+      <div className="absolute inset-0  flex mx-auto items-center justify-center z-1"> 
+        <h1 className='text-white pointer-events-none text-3xl md:text-center' style={{ marginTop: '-8%', marginBottom: '20%', zIndex: 2 }}>Page Not Found</h1>
       </div>
 
       <div className='absolute inset-0 flex items-center justify-center'>
-        <a href="/" className='bg-white translate-y-44 py-2 px-6 rounded-3xl text-black' style={{ zIndex: 1 }}>
+        <a href="/" className='bg-white translate-y-44 py-2 px-8 rounded-3xl text-black' style={{ zIndex: 1 }}>
           Go Home
         </a>
       </div>
