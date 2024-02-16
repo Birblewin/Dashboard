0a45961f38ebabe9035166db41cee65ebe5c884e
itsbluejelly <ronniedenzel0@gmail.com>
2024-02-16 21:53:58 +0300

    removed navbar and footer

--- dashboard/src/app/layout.tsx
+++ dashboard/src/app/layout.tsx
@@ -3,8 +3,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 // IMPORTING COMPONENTS
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/footer";
 import ProgressBar from "@/components/ProgressBar"
 
 const inter = Inter({ subsets: ["latin"] });
@@ -24,14 +22,10 @@ export default function RootLayout({
       <body
         className={`${inter.className} transition-all duration-500 ease-in-out bg-[#050012]`}
       >
-        <Navbar />
-        
         <main className={`min-h-[100vh]`}>
           <ProgressBar/>
           {children}
         </main>
-        
-        <Footer />
       </body>
     </html>
   );
