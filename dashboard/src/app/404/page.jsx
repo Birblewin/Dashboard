021cee7165c0405d56dd3cdc3839ee0e87908545
Ray-Ezra <134621588+Ray-Ezra@users.noreply.github.com>
2024-02-18 15:29:23 +0300

    Update page.jsx
    fixed

--- dashboard/src/app/404/page.jsx
+++ dashboard/src/app/404/page.jsx
@@ -24,7 +24,7 @@ const Train = () => {
         />
         <Image
           loading="lazy"
-          className="absolute transform rotate-90 sm:rotate-0 transition duration-500 md:w-1/2 z-[1]"
+          className="absolute transform rotate-90 sm:rotate-0 transition duration-500 md:w-3/4 md:object-contain z-[1]"
           src='/404.png'
           width={500}
           height={300}
@@ -41,15 +41,15 @@ const Train = () => {
         <h1 className='mt-10 text-3xl -translate-y-[18rem] md:text-4xl md:-translate-y-[12rem] pointer-events-none'>Page Not Found</h1>
         <Image
           loading="lazy"
-          className="absolute w-1/2 md:w-1/4 md:object-contain md:h-1/2 md:translate-y-8"
+          className="absolute w-1/2 md:w-1/4 md:object-contain md:h-3/4 md:translate-y-8"
           src="/Astronaut.png"
           width={500}
           height={300}
         />
-        <Link href="/" className='absolute z-10 bg-white text-black py-2 px-6 text-sm md:text-lg rounded-3xl translate-y-[14rem] md:translate-y-[13rem]
-        '>
+        <Link href="/" class='absolute z-10 bg-white text-black py-2 px-6 text-sm md:text-lg rounded-3xl -translate-y-[2rem] md:translate-y-[10rem] md:-translate-x-[1rem] bottom-10 md:bottom-auto left-1/2 transform -translate-x-1/2 md:static'>
           Go Home
         </Link>
+
       </div>
     </>
 
