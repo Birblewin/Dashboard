ca417cd3eac6f865b742871b6aab81663f1c516b
Ray-Ezra <raymondezra276@gmail.com>
2024-02-18 12:13:11 +0300

    updated

--- dashboard/src/app/404/page.jsx
+++ dashboard/src/app/404/page.jsx
@@ -3,45 +3,57 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 const Background = () => {
-    return (
-        <>
-            <div className="min-h-screen inset-0 flex flex-col items-center justify-center fixed z-10">
-                <Image
-                    loading="lazy"
-                    className="absolute bg-[url('/Small-Planets.png')] bg-cover object-contain bg-no-repeat bg-center w-full h-full"
-                />
-                <Image
-                    loading="lazy"
-                    className="absolute bg-[url('/Stars.png')] bg-cover object-contain bg-no-repeat bg-center w-full h-full"
-                />
-                <Image
-                    loading="lazy"
-                    className="absolute bg-[url('/404.png')] bg-contain bg-no-repeat bg-center transform rotate-90 sm:rotate-0 transition duration-500"
-                    width={500}
-                    height={300}
-                />
-                <Image
-                    loading="lazy"
-                    className="absolute bg-[url('/Big-Planet.png')] bg-contain bg-no-repeat bg-center mx-auto w-full translate-y-3/4  md:w-3/4 md:md:translate-y-[24rem] "
-                    width={500}
-                    height={500}
-                />
-            </div>
-            <div className="min-h-screen inset-0 flex flex-col items-center justify-center fixed z-20">
-                <h1 className='mt-10 text-2xl -translate-y-[18rem] md:text-4xl md:-translate-y-[10rem] pointer-events-none'>Page Not Found</h1>
-                <Image
-                    loading="lazy"
-                    className="absolute bg-[url('/Astronaut.png')] bg-contain bg-no-repeat bg-center sm:translate-y-[2rem]"
-                    width={500}
-                    height={300}
-                />
-                <Link href="/" className='absolute z-10 bg-white text-black py-2 px-8 rounded-3xl translate-y-[16rem] translate-x-[2rem] md:translate-y-[11rem] md:-translate-x-[1rem]'>
-                    Go Home
-                </Link>
-            </div>
-        </>
+  return (
+    <>
+      <div className="min-h-screen inset-0 flex flex-col items-center justify-center fixed z-10">
+        <div
+          loading="lazy"
+          className="absolute bg-[url('/Small-Planets.png')] bg-cover object-contain bg-no-repeat bg-center w-full h-full"
+        />
+        <div
+          loading="lazy"
+          className="absolute bg-[url('/Stars.png')] bg-cover object-contain bg-no-repeat bg-center w-full h-full"
+        />
+        <div
+          loading="lazy"
+          className="absolute bg-[url('/left-Gradient.png')] bg-cover object-contain bg-no-repeat bg-center w-full h-full"
+        />
+        <div
+          loading="lazy"
+          className="absolute bg-[url('/right-Gradient.png.png')] bg-cover object-contain bg-no-repeat bg-center w-full h-full top-0 right-0"
+        />
+        <Image
+          loading="lazy"
+          className="absolute transform rotate-90 sm:rotate-0 transition duration-500 z-[1]"
+          src='/404.png'
+          width={500}
+          height={300}
+        />
+        <Image
+          loading="lazy"
+          className="absolute mx-auto w-full translate-y-full  md:w-3/4 md:md:translate-y-[24rem] "
+          src='/Big-Planet.png'
+          width={500}
+          height={500}
+        />
+      </div>
+      <div className="m-h-screen inset-0 flex flex-col items-center justify-center fixed z-10">
+        <h1 className='mt-10 text-3xl -translate-y-[18rem] md:text-4xl md:-translate-y-[10rem] pointer-events-none'>Page Not Found</h1>
+        <Image
+          loading="lazy"
+          className="absolute w-1/2 md:w-1/4 md:object-contain md:h-1/2 md:translate-y-8"
+          src="/Astronaut.png"
+          width={500}
+          height={300}
+        />
+        <Link href="/" className='absolute z-10 bg-white text-black py-2 px-6 text-sm md:text-lg rounded-3xl translate-y-[14rem] md:translate-y-[11rem]
+        '>
+          Go Home
+        </Link>
+      </div>
+    </>
 
-    );
+  );
 };
 
 export default Background;
