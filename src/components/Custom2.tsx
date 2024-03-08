98c6aa69a3e25afc1ed0f95f94dc3e6b630b4632
Denis Mwangangi <dennomwangs1978@gmail.com>
2024-03-08 18:43:15 +0300

    Custom C source code , formcontrol and full functionality added

--- src/components/Custom2.tsx
+++ src/components/Custom2.tsx
@@ -1,8 +1,112 @@
 "use client"
 
+import { CustomCName, CustomCLicense, CustomCPauseable, CustomCUpgradeable, CustomCAccessControl, CustomCUpgradeability, CustomCUpgradeabilityTransparent, CustomCUpgradeabilityUUPS, CustomCAccessControlRoles, CustomCAccessControlOwnable } from "@/store/CustomC";
+import { useRecoilState } from "recoil";
 import Tool from "./Tool";
 
 export default function Custom2() {
+    const [name, setName] = useRecoilState(CustomCName);
+    const [license, setLicense] = useRecoilState(CustomCLicense);
+    const [pauseable, setPauseable] = useRecoilState(CustomCPauseable);
+    const [upgradeable, setUpgradeable] = useRecoilState(CustomCUpgradeable)
+    const [accessControl, setAccessControl] = useRecoilState(CustomCAccessControl);
+    const [upgradeability, setUpgradeability] = useRecoilState(CustomCUpgradeability);
+    const [transparent, setTransparent] = useRecoilState(CustomCUpgradeabilityTransparent);
+    const [UUPS, setUUPS] = useRecoilState(CustomCUpgradeabilityUUPS);
+    const [roles, setRoles] = useRecoilState(CustomCAccessControlRoles);
+    const [ownable, setOwnable] = useRecoilState(CustomCAccessControlOwnable);
+
+    const handleAccessControlChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value;
+        const isChecked = e.target.checked;
+        
+        if (value === 'ownable') {
+            setOwnable(true);
+            setRoles(false);
+            
+        } else if (value === 'roles') {
+            setRoles(true);
+            setOwnable(false);
+
+        } 
+        
+        // If either Burnable or Pauseable is true, set Access Control to true
+    
+        if (isChecked || pauseable || upgradeable) {
+            setAccessControl(true);
+            
+        } else {
+            setAccessControl(false);
+        }
+        
+    };
+    
+    
+    const handlePauseableChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setPauseable(!pauseable);
+        const isChecked = e.target.checked;
+        
+        // If Pauseable is true, set Access Control to true
+        if (isChecked) {
+            setAccessControl(true);
+            setOwnable(true)
+            
+        } else {
+            // If both Burnable and Pauseable are false, set Access Control to false
+            if (!upgradeable) {
+                setAccessControl(false);
+                setOwnable(false)
+                setRoles(false)
+            }
+        }
+    };
+
+    const handleUpgradeable = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setUpgradeable(!upgradeable);
+        const isChecked = e.target.checked;
+        
+        // If Pauseable is true, set Access Control to true
+        if (isChecked) {
+            setAccessControl(true);
+            setOwnable(true)
+            
+        } else {
+            // If both Burnable and Pauseable are false, set Access Control to false
+            if ( !pauseable) {
+                setAccessControl(false);
+                setOwnable(false)
+                setRoles(false)
+            }
+        }
+    };
+
+    
+
+      const handleAccessControl = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value;
+        const isChecked = e.target.checked;
+
+        if (!pauseable) {
+        
+            if (value === 'access') {
+            
+            if (isChecked) {
+                setOwnable(true);
+                setRoles(false);
+    
+            } else {
+                setOwnable(false);
+                setRoles(false);
+                
+            }
+            setAccessControl(!accessControl);
+            }
+        }else {
+            // If either Mintable or Pauseable is true, prevent the change in Access Control
+            e.preventDefault();
+        }
+      };
+
     return (
         <div className=" flex flex-col rounded-lg overflow-y-visible p-4 bg-white ">
           <h2 className="text-[#818998] font-semibold text-xs">SETTINGS</h2>
@@ -12,6 +116,7 @@ export default function Custom2() {
                     type="text"
                     placeholder="MyContract"
                     className="mt-2 p-1 rounded border border-gray-300 text-black"
+                    value={name}  onChange={(e) => setName(e.target.value)}
                 />
             </form>
             <hr className="my-4"></hr>
@@ -23,7 +128,9 @@ export default function Custom2() {
                     <input
                         title="Pausable"
                         type="checkbox"
-                        className="form-checkbox h-3 w-3 text-indigo-600 rounded"   
+                        className="form-checkbox h-3 w-3 text-indigo-600 rounded" 
+                        checked={pauseable}
+                        onChange={handlePauseableChange}  
                     />
                     <label className="ml-[0.5rem] text-[#333333]">Pausable</label>
                     </div>
@@ -35,7 +142,8 @@ export default function Custom2() {
                         title="Upgradeable"
                         type="checkbox"
                         className="form-checkbox h-3 w-3 text-indigo-600 rounded"
-
+                        checked={upgradeable}
+                        onChange={handleUpgradeable}
                     />
                     <label className="ml-[0.5rem] text-[#333333] ">Upgradeable</label>
                     </div>
@@ -51,6 +159,9 @@ export default function Custom2() {
                         title="Ownable"
                         type="checkbox"
                         className="form-checkbox h-3 w-3 rounded"
+                        checked={accessControl}
+                            value="access"
+                            onChange={handleAccessControl}
                     />
                     </div>
                 </div>
@@ -60,6 +171,9 @@ export default function Custom2() {
                         title="Ownable"
                         type="radio"
                         className="form-checkbox h-3 w-3"
+                        value="ownable"
+                        checked={ownable}
+                        onChange={handleAccessControlChange}
 
                     />
                     <label className="ml-2 text-[#333333] ">Ownable</label>
@@ -72,6 +186,9 @@ export default function Custom2() {
                         title="Roles"
                         type="radio"
                         className="radio h-3 w-3 "
+                        value="roles"
+                        checked={roles}
+                        onChange={handleAccessControlChange}
                     />
                     <label className="ml-2 text-[#333333] ">Roles</label>
                     </div>
@@ -80,50 +197,12 @@ export default function Custom2() {
             </div>
             <hr className="my-4" />
             <div>
-                <div className=" flex">
-                    <div className=" flex items-center">
-                    <label className="text-[#818998] font-semibold text-xs mr-[0.5rem]">UPGRADEABILITY</label>
-                    <input
-                        title="upgradability"
-                        type="checkbox"
-                        className="form-checkbox h-3 w-3 rounded"
-
-                    />
-                    </div>
-                </div>
-                <div className="m-[0.5rem] flex items-center justify-between">
-                    <div  className="flex items-center">
-                    <input
-                        title="transparent"
-                        type="radio"
-                        className="form-checkbox h-3 w-3"
-
-                    />
-                    <label className="ml-2 text-[#333333] ">Transparent</label>
-                    </div>
-                    <Tool tooltipText="Tooltip Text Here" link='https://www.birbleai.com/' linktext='Read more'/>
-                </div>
-                <div className="m-[0.5rem] mb-0 flex items-center justify-between">
-                    <div className=" flex items-center">
-                    <input
-                        title="uups"
-                        type="radio"
-                        className="radio h-3 w-3" 
-                    />
-                    <label className="ml-2 text-[#333333] ">UUPS</label>
-                    </div>
-                    <Tool tooltipText="Tooltip Text Here" link='https://www.birbleai.com/' linktext='Read more'/>
-                </div>
-
-            </div>
-            <hr className="my-4" />
-            <div>
                 <h1 className="text-[#818998] font-semibold text-xs ">INFO</h1>
                 <div className="flex flex-col mt-[0.75rem]">
                     <label htmlFor="license" className="text-[#333333] text-sm">
                         License
                     </label>
-                    <input  id="license" type="text" placeholder="MIT" className="border border-1 border-[#333333] rounded-[6px] p-1  text-black" />
+                    <input  id="license" type="text" placeholder="MIT" className="border border-1 border-[#333333] rounded-[6px] p-1  text-black" value={license}  onChange={(e) => setLicense(e.target.value)}/>
                 </div>
             </div>
         </div>
--- src/generator/CustomC.ts
+++ src/generator/CustomC.ts
@@ -3,6 +3,7 @@ import CustomCCode from "@/code/CustomC";
 
 
 
+
 interface CodeSection {
     section: string;
     tag?: string;
@@ -12,83 +13,177 @@ const getCodeContent = (section: string, tag?: string): string[] => {
     return CustomCCode.filter((code: CodeSection) => code.section === section && (!tag || code.tag === tag)).map(filteredCode => filteredCode.content);
 }
 
-export const License: string[] = getCodeContent("license");
-export const Compatibility: string[] = getCodeContent("compatibility");
-export const CodeVersion: string[] = getCodeContent("codeVersion");
-export const StartUpgradeableImport: string[] = getCodeContent("upgradeableImports", "DefaultStart");
-export const BurnableUpgradeableImport: string[] = getCodeContent("upgradeableImports", "Burnable");
-export const PausableUpgradeableImport: string[] = getCodeContent("upgradeableImports", "Pausable");
-export const FlashMintingUpgradeableImport: string[] = getCodeContent("upgradeableImports", "flashMinting");
-export const PermitUpgradeableImport: string[] = getCodeContent("upgradeableImports", "Permit");
-export const VotesUpgradeableImport: string[] = getCodeContent("upgradeableImports", "Votes");
-export const OwnableUpgradeableImport: string[] = getCodeContent("upgradeableImports", "Ownable");
-export const RolesUpgradeableImport: string[] = getCodeContent("upgradeableImports", "Roles");
-export const ManagedUpgradeableImport: string[] = getCodeContent("upgradeableImports", "Managed");
-export const EndUpgradeableImport: string[] = getCodeContent("upgradeableImports", "DefaultEnd");
-export const UUPSUpgradeableImport: string[] = getCodeContent("upgradeableImports", "UUPS");
-export const Import: string[] = getCodeContent("Imports", "Default");
-export const BurnableImport: string[] = getCodeContent("Imports", "Burnable");
-export const PausableImport: string[] = getCodeContent("Imports", "Pausable");
-export const FlashMintingImport: string[] = getCodeContent("Imports", "flashMinting");
-export const PermitImport: string[] = getCodeContent("Imports", "Permit");
-export const VotesImport: string[] = getCodeContent("Imports", "Votes");
-export const OwnableImport: string[] = getCodeContent("Imports", "Ownable");
-export const RolesImport: string[] = getCodeContent("Imports", "Roles");
-export const ManagedImport: string[] = getCodeContent("Imports", "Managed");
-export const SecurityContact: string[] = getCodeContent("SecurityContact");
-export const ContractHeader: string[] = getCodeContent("ContractHeader");
-export const ContractName: string[] = getCodeContent("ContractNames", "Default");
-export const OwnableContractName: string[] = getCodeContent("ContractNames", "Ownable");
-export const RolesContractName: string[] = getCodeContent("ContractNames", "Roles");
-export const ManagedContractName: string[] = getCodeContent("ContractNames", "Managed");
-export const BurnableContractName: string[] = getCodeContent("ContractNames", "Burnable");
-export const PausableContractName: string[] = getCodeContent("ContractNames", "Pausable");
-export const VotesContractName: string[] = getCodeContent("ContractNames", "Votes");
-export const PermitContractName: string[] = getCodeContent("ContractNames", "Permit");
-export const FlashMintContractName: string[] = getCodeContent("ContractNames", "flashMinting");
-export const UpgradeableContractName: string[] = getCodeContent("upgradeableContractNames", "Default");
-export const OwnableUpgradeableContractName: string[] = getCodeContent("upgradeableContractNames", "Ownable");
-export const RolesUpgradeableContractName: string[] = getCodeContent("upgradeableContractNames", "Roles");
-export const ManagedUpgradeableContractName: string[] = getCodeContent("upgradeableContractNames", "Managed");
-export const BurnableUpgradeableContractName: string[] = getCodeContent("upgradeableContractNames", "Burnable");
-export const PermitUpgradeableContractName: string[] = getCodeContent("upgradeableContractNames", "Permit");
-export const VotesUpgradeableContractName: string[] = getCodeContent("upgradeableContractNames", "Votes");
-export const PausableUpgradeableContractName: string[] = getCodeContent("upgradeableContractNames", "Pausable");
-export const FlashMintUpgradeableContractName: string[] = getCodeContent("upgradeableContractNames", "flashMinting");
-export const RolesByte: string[] = getCodeContent("RolesByte");
-export const DefaultConstructor: string[] = getCodeContent("Constructor", "Default"); 
-export const PermitConstructor: string[] = getCodeContent("Constructor", "Permit");
-export const OwnableConstructor: string[] = getCodeContent("Constructor", "Ownable");
-export const RolesConstructor: string[] = getCodeContent("Constructor", "Roles");
-export const ManagedConstructor: string[] = getCodeContent("Constructor", "Managed");
-export const UpgradeableConstructor: string[] = getCodeContent("upgradeableConstructor");
-export const MintableSection2: string[] = getCodeContent("Section2", "Mintable");
-export const PausableSection2: string[] = getCodeContent("Section2", "Pausable");
-export const UUPSSection2: string[] = getCodeContent("Section2", "UUPS");
-export const PausableSection3: string[] = getCodeContent("Section3", "Pausable");
-export const VotesSection3: string[] = getCodeContent("Section3", "Votes");
-export const Section1Header: string[] = getCodeContent("upgradeableFunctionsHeader", "Default");
-export const PausableSection1Header: string[] = getCodeContent("upgradeableFunctionsHeader", "Pausable");
-export const MintableSection1Header: string[] = getCodeContent("upgradeableFunctionsHeader", "Mintable");
-export const Section1: string[] = getCodeContent("upgradeableFunctions", "Default");
-export const OwnableSection1: string[] = getCodeContent("upgradeableFunctions", "Ownable");
-export const RolesSection1: string[] = getCodeContent("upgradeableFunctions", "Roles");
-export const ManagedSection1: string[] = getCodeContent("upgradeableFunctions", "Managed");
-export const BurnableSection1: string[] = getCodeContent("upgradeableFunctions", "Burnable");
-export const PausableSection1: string[] = getCodeContent("upgradeableFunctions", "Pausable");
-export const PermitSection1: string[] = getCodeContent("upgradeableFunctions", "Permit");
-export const VotesSection1: string[] = getCodeContent("upgradeableFunctions", "Votes");
-export const FlashMintingSection1: string[] = getCodeContent("upgradeableFunctions", "flashMinting");
+ const License: string[] = getCodeContent("license");
+ const Compatibility: string[] = getCodeContent("compatibility");
+const PausableVariable: string[] = getCodeContent("Variables", "Pausable");
+const UpgradeableVariable: string[] = getCodeContent("Variables", "Upgradeable");
+const WizardTag: string[] = getCodeContent("WizardTag");
+const PausableZeppelinImport: string[] = getCodeContent("ZeppelinImports", "Pausable");
+const OwnableZeppelinImport: string[] = getCodeContent("ZeppelinImports", "Ownable");
+const RolesZeppelinImport: string[] = getCodeContent("ZeppelinImports", "Roles");
+const UpgradeableZeppelinImport: string[] = getCodeContent("ZeppelimImports", "Upgradeable");
+const UpgradeableStarknetImport: string[] = getCodeContent("StarknetImports", "Upgradeable");
+const AccessControlStarknetImport: string[] = getCodeContent("StarknetImports", "AccessControl");
+const SuperImport: string[] = getCodeContent("SuperImports", "Default");
+ const PausableSuperImport: string[] = getCodeContent("SuperImports", "Pausable");
+ const UpgradeableSuperImport: string[] = getCodeContent("SuperImports", "Upgradeable");
+ const ContractHeader: string[] = getCodeContent("ContractHeader");
+ const Component: string[] = getCodeContent("Components", "Default");
+ const PausableComponent: string[] = getCodeContent("Components", "Pausable");
+ const UpgradeableComponent: string[] = getCodeContent("Components", "Upgradeable");
+ const PausableEmbed: string[] = getCodeContent("Embeds", "Pausable");
+ const OwnableEmbed: string[] = getCodeContent("Embeds", "Ownable");
+ const RolesEmbed: string[] = getCodeContent("Embeds", "Roles");
+ const PausableIMPL: string[] = getCodeContent("IMPLS", "Pausable");
+ const OwnableIMPL: string[] = getCodeContent("IMPLS", "Ownable");
+ const RolesIMPL: string[] = getCodeContent("Components", "Roles");
+ const UpgradeableIMPL: string[] = getCodeContent("IMPLS", "Upgradeable");
+ const Storage: string[] = getCodeContent("Storage", "Default");
+ const PausableStorage: string[] = getCodeContent("Storage", "Pausable");
+ const OwnableStorage: string[] = getCodeContent("Storage", "Ownable");
+ const RolesStorage: string[] = getCodeContent("Storage", "Roles");
+ const UpgradeableStorage: string[] = getCodeContent("Storage", "Upgradeable");
+ const Event: string[] = getCodeContent("Events", "Default");
+ const OwnableEvent: string[] = getCodeContent("Events", "Ownable");
+ const RolesEvent: string[] = getCodeContent("Events", "Roles");
+ const PausableEvent: string[] = getCodeContent("Events", "Pausable");
+ const UpgradeableEvent: string[] = getCodeContent("Events", "Upgradeable");
+ const Constructor: string[] = getCodeContent("Constructor", "Default");
+ const PausableConstructor: string[] = getCodeContent("Constructor", "Pausable");
+ const OwnableConstructor: string[] = getCodeContent("Constructor", "Ownable");
+ const RolesConstructor: string[] = getCodeContent("Constructor", "Roles");
+ const UpgradeableConstructor: string[] = getCodeContent("Constructor", "Upgradeable");
+ const ConstructorReturn: string[] = getCodeContent("ConstructorReturns", "Default");
+ const OwnableConstructorReturns: string[] = getCodeContent("ConstructorReturns", "Ownable");
+ const RolesConstructorReturns: string[] = getCodeContent("ConstructorReturns", "Roles");
+ const PausableConstructorReturns: string[] = getCodeContent("ConstructorReturns", "Pausable");
+ const UpgradeableConstructorReturns: string[] = getCodeContent("ConstructorReturns", "Upgradeable");
+ const Pausable1Trait: string[] = getCodeContent("Traits", "Pausable1");
+ const Pausable2Trait: string[] = getCodeContent("Traits", "Pausable2");
+ const OwnableUpgradeableABI: string[] = getCodeContent("UpgradeableABI", "Ownable");
+ const RolesUpgradeableABI: string[] = getCodeContent("UpgradeableABI", "Roles");
 
 
    
   
-export function generateCustomCCode(customcupgradeable: boolean,customcpausable : boolean, customcroles : boolean, customcownable : boolean,  customcupgradeability : boolean, customcUUPS : boolean): string {
-   
+export function generateCustomCCode(customcupgradeable: boolean,customcpausable: boolean, customcroles: boolean, customcownable: boolean,  customcupgradeability: boolean, customcUUPS: boolean): string {
+    const Variables = [
+    customcpausable? PausableVariable : "",
+    customcupgradeable? UpgradeableVariable : ""
+    
+  ].filter(Boolean).join('\n').trim();
+
+  const  Zeppelin = [
+    
+    customcpausable? PausableZeppelinImport: "",
+    customcownable ? OwnableZeppelinImport: "",
+    customcroles? RolesZeppelinImport: "",
+    customcupgradeable? UpgradeableZeppelinImport: ""
+  ].filter(Boolean).join('\n').trim(); 
+  
+  const  Starknet = [
+    
+    customcownable || customcroles  ? AccessControlStarknetImport: "",
+    customcupgradeable? UpgradeableStarknetImport: ""
+  ].filter(Boolean).join('\n').trim(); 
+
+  const  Superimport = [
+    SuperImport,
+    customcpausable? PausableSuperImport: "",
+    
+    customcupgradeable? UpgradeableSuperImport: ""
+  ].filter(Boolean).join('\n').trim(); 
+
+  const  Components = [
+    Component,
+    customcpausable? PausableComponent: "",
+    customcownable  ? OwnableZeppelinImport: "",
+    customcroles ? RolesZeppelinImport: "",
+    customcupgradeable ? UpgradeableComponent: ""
+  ].filter(Boolean).join('\n').trim(); 
+
+  const  Embeds = [
+    customcpausable? PausableEmbed: "",
+    customcownable  ? OwnableEmbed: "",
+    customcroles ? RolesEmbed: "",
+    
+  ].filter(Boolean).join('\n').trim(); 
+
+  const IMPLS = [
+    customcpausable? PausableIMPL : "",
+    customcownable? OwnableIMPL: "",
+    customcroles ? RolesIMPL: "",
+    customcupgradeable? UpgradeableIMPL : ""
+
+  ].filter(Boolean).join('\n').trim();
+
+
+  const Storages = [
+    Storage,
+    customcpausable? PausableStorage : "",
+    customcownable? OwnableStorage: "",
+    customcroles ? RolesStorage: "",
+    customcupgradeable? UpgradeableStorage : "",
+    "}"
+  ].filter(Boolean).join('\n').trim();
+
+  const Events = [
+    Event,
+    customcpausable? PausableEvent : "",
+    customcownable? OwnableEvent: "",
+    customcroles ? RolesEvent: "",
+    customcupgradeable? UpgradeableEvent : "",
+    "}"
+  ].filter(Boolean).join('\n').trim();
+
+  const  Constructors = [
+    Constructor,    
+    customcownable ? OwnableConstructor: "",
+    customcroles? RolesConstructor: "",
+    customcpausable && customcroles? PausableConstructor: "",
+    customcupgradeable? UpgradeableConstructor: "",
+    "){"
+  ].filter(Boolean).join('\n').trim(); 
+
+  const  ConstructorReturns = [
+    ConstructorReturn,    
+    customcownable ? OwnableConstructorReturns: "",
+    customcroles? RolesConstructorReturns: "",
+    customcpausable && customcroles? PausableConstructorReturns: "",
+    customcupgradeable && customcroles? UpgradeableConstructorReturns: "",
+    "}"
+  ].filter(Boolean).join('\n').trim(); 
+
+  
+  const Traits = [
+    customcpausable && customcownable ? Pausable1Trait: "",
+    customcpausable && customcroles ? Pausable2Trait: "",
+  ].filter(Boolean).join('\n').trim();
+
+  const UpgradeableABIs = [
+    customcownable? OwnableUpgradeableABI: "",
+    customcroles? RolesUpgradeableABI: ""
+  ].filter(Boolean).join('\n').trim();
+
+
   const result =[
     License,
     Compatibility,
-    CodeVersion,
+    Variables,
+    WizardTag,
+    ContractHeader,
+    Zeppelin,
+    Starknet,
+    Superimport,
+    Components,
+    Embeds,
+    IMPLS,
+    Storages,
+    Events,
+    Constructors,
+    ConstructorReturns,
+    customcpausable ?  Traits: "",
+    customcupgradeable? UpgradeableABIs : ""
    
   ].filter(Boolean).join('\n'); 
 
