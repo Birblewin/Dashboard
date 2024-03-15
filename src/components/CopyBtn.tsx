3f2efda2775548a26b7c9f193b39f667a3bd607a
Denis Mwangangi <dennomwangs1978@gmail.com>
2024-03-15 17:25:19 +0300

    copy functionality added

--- src/components/CopyBtn.tsx
+++ src/components/CopyBtn.tsx
@@ -1,13 +1,40 @@
 "use client"
 
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import CheckIcon from '@mui/icons-material/Check';
+import { useState } from 'react';
+import copy from 'clipboard-copy';
+
 
 function CopyBtn() {
+  const [isCopied, setIsCopied] = useState(false);
+  const text = "code"
+
+  const handleCopyClick = async () => {
+    try {
+      await copy(text);
+      setIsCopied(true);
+      setTimeout(() => {
+        setIsCopied(false);
+      }, 3000);
+    } catch (error) {
+      console.error('Failed to copy text to clipboard', error);
+    }
+  };
   return (
     <div>
-       <button className="font-bold text-black border p-2 rounded hover:bg-gray-200 flex gap-1">
-            <ContentCopyIcon width={12} height={12}/>
-            Copy to clipboard
+       <button className="font-bold text-black border p-2 rounded hover:bg-gray-200 flex gap-1" onClick={handleCopyClick}>
+          {isCopied ? (
+            <>
+              <CheckIcon width={12} height={12} />
+              Copied
+            </>
+          ) : (
+            <>
+              <ContentCopyIcon width={12} height={12} />
+              Copy to clipboard
+            </>
+          )}
         </button>
     </div>
   )
