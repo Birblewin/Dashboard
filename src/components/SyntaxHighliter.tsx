db886ee695d290649b96727c9e3f892634fee912
Denis Mwangangi <dennomwangs1978@gmail.com>
2024-03-22 22:06:08 +0300

    added underline style to links

--- src/components/SyntaxHighliter.tsx
+++ src/components/SyntaxHighliter.tsx
@@ -238,7 +238,7 @@ const CodeEditor: React.FC = () => {
         // We are modifying HTML, so use HTML escaped chars. The pattern excludes paths that include /../ in the URL.
         const importRegex = /&quot;(@openzeppelin\/)(contracts-upgradeable\/|contracts\/)((?:(?!\.\.)[^/]+\/)*?[^/]*?)&quot;/g
       
-        return code.replace(importRegex, `&quot;<a class="import-link" href="https://github.com/OpenZeppelin/openzeppelin-$2blob/v5.0.2/contracts/$3" target="_blank" rel="noopener noreferrer">$1$2$3</a>&quot;`);
+        return code.replace(importRegex, `&quot;<a class="import-link" style="text-decoration: underline;" href="https://github.com/OpenZeppelin/openzeppelin-$2blob/v5.0.2/contracts/$3" target="_blank" rel="noopener noreferrer">$1$2$3</a>&quot;`);
       }
 
       const injectedCode = injectHyperlinks(hljs.highlight('solidity', initialCode).value)
