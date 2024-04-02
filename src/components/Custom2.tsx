a804cc8be7010f95679305054a8c15f4047618d8
Denis Mwangangi <dennomwangs1978@gmail.com>
2024-04-02 15:29:34 +0300

    cairo contracts dark theme UI

--- src/components/Custom2.tsx
+++ src/components/Custom2.tsx
@@ -48,9 +48,10 @@ export default function Custom2() {
         
         // If Pauseable is true, set Access Control to true
         if (isChecked) {
+          if(!accessControl){
             setAccessControl(true);
             setOwnable(true)
-            
+          }  
         } else {
             // If both Burnable and Pauseable are false, set Access Control to false
             if (!upgradeable) {
@@ -67,9 +68,10 @@ export default function Custom2() {
         
         // If Pauseable is true, set Access Control to true
         if (isChecked) {
+          if(!accessControl){
             setAccessControl(true);
             setOwnable(true)
-            
+          }  
         } else {
             // If both Burnable and Pauseable are false, set Access Control to false
             if ( !pauseable) {
@@ -110,13 +112,13 @@ export default function Custom2() {
     return (
         <div className="p-4 overflow-visible">
         <div>
-            <h1 className="text-[#818998] font-semibold text-xs">SETTINGS</h1>
+            <h1 className="text-white font-semibold text-xs">SETTINGS</h1>
             <form className="flex flex-col">
-                <label className="text-black mt-2 text-xs font-normal">Name</label>
+                <label className="text-white mt-2 text-xs font-normal">Name</label>
                 <input
                     type="text"
                     placeholder="MyContract"
-                    className="mt-2 p-1 rounded border border-gray-300 text-black"
+                    className="mt-2 p-1 rounded border bg-transparent border-[#818998] "
                     value={name}  onChange={(e) => setName(e.target.value)}
                 />
             </form>
@@ -125,11 +127,11 @@ export default function Custom2() {
         <hr className="my-4"></hr>
   
         <div>  
-          <h1 className="text-[#818998] font-semibold text-xs">FEATURES</h1>
+          <h1 className="text-white font-semibold text-xs">FEATURES</h1>
         <div className="mt-2">
           
   
-          <label className={`flex items-center justify-between cursor-pointer ${pauseable ? 'bg-gray-100' : ''}`}>
+          <label className={`flex items-center justify-between cursor-pointer ${pauseable ? 'bg-[#4D3C77]' : ''}`}>
             <div className="m-[0.5rem] flex items-center">
               <input
                 title="Pauseable"
@@ -138,14 +140,14 @@ export default function Custom2() {
                 checked={pauseable}
                 onChange={handlePauseableChange}
               />
-              <span className="ml-[0.5rem] text-[#333333] select-text">Pauseable</span>
+              <span className="ml-[0.5rem] text-white select-text">Pauseable</span>
             </div>
             <Tool tooltipText="Privileged accounts will be able to pause the functionality marked with self.pausable.assert_not_paused(). Useful for emergency response." link='https://docs.openzeppelin.com/contracts-cairo/0.11.0/security#pausable' linktext='Read more'/>
           </label>
   
           
   
-          <label className={`flex items-center justify-between cursor-pointer ${upgradeable ? 'bg-gray-100' : ''}`}>
+          <label className={`flex items-center justify-between cursor-pointer ${upgradeable ? 'bg-[#4D3C77]' : ''}`}>
             <div className="m-[0.5rem] flex items-center">
               <input
                 title="Updateable URI"
@@ -154,7 +156,7 @@ export default function Custom2() {
                 checked={upgradeable}
                 onChange={handleUpgradeable}
               />
-              <span className="ml-[0.5rem] text-[#333333] select-text">Upgradeable</span>
+              <span className="ml-[0.5rem] text-white select-text">Upgradeable</span>
             </div>
             <Tool tooltipText="Smart contracts are immutable by default unless they include code that allows them to be upgraded." link='https://docs.openzeppelin.com/contracts-cairo/0.11.0/upgrades' linktext='Read more'/>
           </label>          
@@ -166,7 +168,7 @@ export default function Custom2() {
         <div>
           <div className=" mt-0  flex items-center place-content-between">
             <div className="flex items-center mr-[0.5rem]">
-              <label className="text-[#818998] font-semibold text-xs mr-[0.5rem]">ACCESS CONTROL</label>
+              <label className="text-white font-semibold text-xs mr-[0.5rem]">ACCESS CONTROL</label>
               <input
                 type="checkbox"
                 className={`form-checkbox h-3 w-3 rounded ${accessControl && (pauseable || upgradeable) ? "accent-grey-300 hover:accent-grey-300" : ""}`}
@@ -179,7 +181,7 @@ export default function Custom2() {
           </div>
   
         <div className="mt-2">
-          <label className={`flex items-center justify-between cursor-pointer ${ownable ? 'bg-gray-100' : ''}`}>
+          <label className={`flex items-center justify-between cursor-pointer ${ownable ? 'bg-[#4D3C77]' : ''}`}>
             <div className="m-[0.5rem] flex items-center">
               <input
                 title="Ownable"
@@ -190,12 +192,12 @@ export default function Custom2() {
                 checked={ownable}
                 onChange={handleAccessControlChange}
               />
-              <span className="ml-2 text-[#333333] select-text">Ownable</span>
+              <span className="ml-2 text-white select-text">Ownable</span>
             </div>
             <Tool tooltipText="Simple mechanism with a single account authorized for all privileged actions." link='https://docs.openzeppelin.com/contracts-cairo/0.11.0/access#ownership_and_ownable' linktext='Read more'/>
           </label>
   
-          <label className={`flex items-center justify-between cursor-pointer ${roles ? 'bg-gray-100' : ''}`}>
+          <label className={`flex items-center justify-between cursor-pointer ${roles ? 'bg-[#4D3C77]' : ''}`}>
             <div className="m-[0.5rem] flex items-center">
               <input
                 title="Roles"
@@ -206,7 +208,7 @@ export default function Custom2() {
                 checked={roles}
                 onChange={handleAccessControlChange}
               />
-              <span className="ml-2 text-[#333333] select-text">Roles</span>
+              <span className="ml-2 text-white select-text">Roles</span>
             </div>
             <Tool tooltipText="Flexible mechanism with a separate role for each privileged action. A role can have many authorized accounts." link='https://docs.openzeppelin.com/contracts-cairo/0.11.0/access#role_based_accesscontrol' linktext='Read more'/>
           </label>
@@ -219,15 +221,15 @@ export default function Custom2() {
   
          
         <div className="px-2">
-          <h1 className="text-[#818998] font-semibold text-xs ">INFO</h1>
+          <h1 className="text-white font-semibold text-xs ">INFO</h1>
           
           <div className="flex flex-col mt-[0.75rem]">
-            <label htmlFor="license" className="text-[#333333] text-sm"> License </label>
+            <label htmlFor="license" className="text-white text-sm"> License </label>
             <input
               id="license"
               type="text"
               placeholder="MIT"
-              className="border border-1 border-[#333333] rounded-[6px] p-1  text-black"
+              className="border border-1 border-[#818998] rounded-[6px] p-1 bg-transparent"
               value={license} onChange={(e) => setLicense(e.target.value)}
             />
           </div>
