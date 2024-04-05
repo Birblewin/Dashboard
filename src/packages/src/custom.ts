f3c7e2ea0db7881a470e61680c56577c20b6a0dc
mosesmkrs <karisamoses392@gmail.com>
2024-04-05 19:12:56 +0300

    correction

--- src/packages/src/custom.ts
+++ src/packages/src/custom.ts
@@ -1,4 +1,4 @@
-import { Contract, ContractBuilder } from './contract';
+import { Contractt, ContractBuilder } from './contract';
 import { CommonOptions, withCommonDefaults, defaults as commonDefaults } from './common-options';
 import { setUpgradeable } from './set-upgradeable';
 import { setInfo } from './set-info';
@@ -35,7 +35,7 @@ export function isAccessControlRequired(opts: Partial<CustomOptions>): boolean {
   return opts.pausable || opts.upgradeable === 'uups';
 }
 
-export function buildCustom(opts: CustomOptions): Contract {
+export function buildCustom(opts: CustomOptions): Contractt {
   const allOpts = withDefaults(opts);
 
   const c = new ContractBuilder(allOpts.name);
